0314f3becb4073ca10281cf1a014443458334502
Student_04 <Student_04@DESKTOP-M6L6ME5>
2026-09-17 12:50:21 +0300

    Lab01:first program with Environment

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using System.Data;
+
+Console.WriteLine($"Компьютер:{Environment.MachineName}");
+Console.WriteLine($"Пользователь:{Environment.UserName}");
+Console.WriteLine($"Дата и время:{DateTime.Now:dd.MM.yyyy HH:mm}");
